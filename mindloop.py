2c57e65d2db8c300c4598fa3410060aa347f2bfd
jakedahn <jake@markupisart.com>
2023-08-04 21:47:04 -0600

    almost working

--- mindloop.py
+++ mindloop.py
@@ -1,9 +1,22 @@
+import os
+
 import openai
-from chromadb import ChromaDB
+
+import chromadb
 from chromadb.utils import embedding_functions
 
+NPC_SYSTEM_PROMPT = '''
+You are an NPC in dungeons and dragons.
+You are interacting with townspeople to attain the item which you desire.
+You and the townspeople need to work together to figure out how to each meet each others goals.
+
+You must respond to all queries using the following persona:
+
+{persona}
+'''
+
 # Initialize the OpenAI API
-openai.api_key = "YOUR_OPENAI_API_KEY"
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 # Setup OpenAI Embedding Function
 openai_ef = embedding_functions.OpenAIEmbeddingFunction(
@@ -11,7 +24,7 @@ openai_ef = embedding_functions.OpenAIEmbeddingFunction(
 )
 
 # Initialize ChromaDB with OpenAI as the embedding function
-db = ChromaDB("./brain.db", embedding_function=openai_ef)
+db = chromadb.Client("./brain.db", embedding_function=openai_ef)
 collection = db.get_or_create_collection("queries_collection")
 
 
@@ -48,38 +61,46 @@ def store_query_response(query, response):
     """
     collection.add(documents=[query], ids=[response])
 
+def prompt_npc(persona, query, relevant_memories):
+    system_prompt = NPC_SYSTEM_PROMPT.format(persona=persona)
+
+
+    npc_prompt = npc_prompt.format(query=query)
+    # Create the list of messages for the chat API
+    messages = [
+        {
+            "role": "system",
+            "content": system_prompt
+        },
+        {"role": "user", "content": query},
+    ]
+    for context in relevant_memories:
+        messages.append({"role": "context", "content": context})
+    
+
+
 
 def mind_loop():
     """
     The main loop where the agent awaits a query, infers, and responds.
     """
-    while True:
-        # 1. Awaits a query
-        query = input("Query: ")
+    # while True:
+    # 1. Awaits a query
+    query = input("Query: ")
 
-        # 2. Convert to embeddings and search memory
-        embedded_query = convert_to_embedding(query)
-        relevant_contexts = get_relevant_queries(embedded_query)
+    # 2. Convert to embeddings and search memory
+    embedded_query = convert_to_embedding(query)
+    relevant_memories = get_relevant_queries(embedded_query)
 
-        # Create the list of messages for the chat API
-        messages = [
-            {
-                "role": "system",
-                "content": "You are an NPC. Your sole purpose is to act as the character you are prompted to be. Respond in character to the query.",
-            },
-            {"role": "user", "content": query},
-        ]
-        for context in relevant_contexts:
-            messages.append({"role": "context", "content": context})
 
-        # 3. Inference
-        response = chat_gpt_inference(messages)
+    # 3. Inference
+    response = prompt_npc('Eldridge Hamilton', query, relevant_memories)
 
-        # 4. Response
-        print("Response:", response)
+    # 4. Response
+    print("Response:", response)
 
-        # 5. Store the query and response
-        store_query_response(query, response)
+    # 5. Store the query and response
+    store_query_response(query, response)
 
 
 # Run the mind loop
